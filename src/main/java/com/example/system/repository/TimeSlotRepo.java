c004a29efe806b6e6c11b66c53e7ede52086f7ac
dhruv <dhruvgupta130@gmail.com>
2025-02-03 09:58:40 +0000

    Features Added

--- src/main/java/com/example/system/repository/TimeSlotRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.system.repository;
-
-import com.example.system.entity.TimeSlot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TimeSlotRepo extends JpaRepository<TimeSlot, Long> {
-}
