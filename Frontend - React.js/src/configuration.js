2acfa2fcc2de36100e3c0b29fa441c66ad2acff6
dhruv <dhruvgupta130@gmail.com>
2025-03-11 18:00:26 +0530

    Updated

--- Frontend - React.js/src/configuration.js
+++ Frontend - React.js/src/configuration.js
@@ -1,3 +1,2 @@
-export const BACKEND_URL = "";
-
-export const GOOGLE_MAPS_API_KEY = "";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const GOOGLE_MAPS_API_KEY = import.meta.env.GOOGLE_MAPS_API_KEY;
